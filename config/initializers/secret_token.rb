6f4e74d060cd06bb50894318a2490080de2543f9
Dmytro Paniutin <oxyda92@gmail.com>
2013-03-20 21:42:59 +0200

    Touragency cours project
    Paniutin Dmytro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tours::Application.config.secret_token = 'b527b63f5583551b9d870edd67f28ad0fcb415d7a0544c698f96102ab585ef31b2d171287ea830487f18a068c72cb2762b8b76b3670530ac143da767e2f10fba'
